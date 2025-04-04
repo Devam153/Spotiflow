e1ce2e9afe02b1c6e72c7068d955ed161e3339fa
Devam Tanwani <devamtanwani@gmail.com>
2025-04-05 00:00:13 +0530

    cd Spotiflow

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
-
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
 
+# Install dependencies
 pip install -r requirements.txt
 
+# Navigate to the Spotiflow directory where manage.py is located
+cd Spotiflow
+
+# Run Django commands
 python manage.py collectstatic --no-input
 python manage.py migrate
